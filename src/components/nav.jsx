fa3c05be887bc75d76c018a66063b1ff861bdec7
NAYANA BENNY <ncmia2@gmail.com>
2024-07-23 12:37:18 +0530

    header changes

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,6 @@
 function nav(){
     return(
-        <nav>
-            <ul className="options">
-                <li><a href="/">Pharmacy</a></li>
-                <li><a href="/about">Consultation</a></li>
-                <li>Lab Test</li>
-            </ul>
-        </nav>
+        <nav></nav>
     )
 }
 
