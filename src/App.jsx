86c13af82be66f6b018c4952969c2a9c840fb3c1
ahynie <163619973+ahynie@users.noreply.github.com>
2024-07-23 12:35:55 +0530

    Home trial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./components/nav.jsx"
 import './App.css'
 import Header from "./components/HEADER.jsx"
+import Home from "./components/home.jsx"
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
     <Header />
     <Nav />
+    <Home/>
     </>
   )
 }
