54bbab285d75b522302cb87e58a4b87d1130a2fd
NAYANA BENNY <ncmia2@gmail.com>
2024-07-23 12:28:20 +0530

    logo

--- src/components/HEADER.jsx
+++ src/components/HEADER.jsx
@@ -4,7 +4,6 @@ function header()
 {
     return (
         <div className="header">
-            <h2>Delivery</h2>
             <img className="logo" src={Logo} alt="logo"></img>
             <h2>
                 Profile
