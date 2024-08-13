2621cf37eb9a0b9007f847474957bd650682c740
ahynie <163619973+ahynie@users.noreply.github.com>
2024-08-13 12:56:27 +0530

    banner

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import banner1 from '../assets/scrollimage1.jpg';  
+import banner1 from '../assets/banner.png';  
 import banner3 from '../assets/scrollimage3.jpg';  
 import banner2 from '../assets/scrollimage2.jpg';    
 
