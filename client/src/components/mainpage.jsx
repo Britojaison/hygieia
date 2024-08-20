c5766ea833f76a8fd242d53d1c937d59d50d12da
Britojaison <britojaison123@gmail.com>
2024-08-20 22:56:03 +0530

    pharma conn

--- client/src/components/mainpage.jsx
+++ client/src/components/mainpage.jsx
@@ -5,41 +5,43 @@ import video from "../assets/video.png"
 import labtests from "../assets/labtests.png"
 import ImageCarousel from './Carousel.jsx'
 import vitamin from "../assets/vitaminb12.jpg"
-function mainpage(){
+import { Outlet, Link } from "react-router-dom";
+
+function mainpage() {
     return (
-        <div>      
+        <div>
             <div className="container">
-        <a href="#" className="button button-1">
-        <img className="pharm" src={Pharmacy} alt="pharmacy"></img>
-            <div>Pharmacy Near Me</div>
-            <div>FIND STORE</div>
-        </a>
-        <a href="#" className="button button-2">
-        <img className="notepad" src={notepad} alt="notepad"></img>
-            <div>Get 15% off on Medicines</div>
-            <div>UPLOAD NOW</div>
-        </a>
-        <a href="#" className="button button-3">
-        <img className="hospital" src={hospital} alt="hospital"></img>
-            <div>Hospital Visit</div>
-            <div>PRE-BOOK</div>
-        </a>
-        <a href="#" className="button button-4">
-        <img className="video" src={video} alt="video"></img>
-            <div>Video Consult</div>
-            <div>IN 15 MIN</div>
-        </a>
-        <a href="#" className="button button-5">
-        <img className="labtests" src={labtests} alt="labtests"></img>
-            <div>Lab Tests</div>
-            <div>AT HOME</div>
-        </a>
-    </div> 
-    <ImageCarousel/>
-    <div className="title"> 
-        <h2>VITAMINS</h2>
-    </div>
-    <div className="product-container">
+                <Link to="/pharm" className="button button-1">
+                    <img className="pharm" src={Pharmacy} alt="pharmacy"></img>
+                    <div>Pharmacy Near Me</div>
+                    <div>FIND STORE</div>
+                </Link>
+                <a href="#" className="button button-2">
+                    <img className="notepad" src={notepad} alt="notepad"></img>
+                    <div>Get 15% off on Medicines</div>
+                    <div>UPLOAD NOW</div>
+                </a>
+                <a href="#" className="button button-3">
+                    <img className="hospital" src={hospital} alt="hospital"></img>
+                    <div>Hospital Visit</div>
+                    <div>PRE-BOOK</div>
+                </a>
+                <a href="#" className="button button-4">
+                    <img className="video" src={video} alt="video"></img>
+                    <div>Video Consult</div>
+                    <div>IN 15 MIN</div>
+                </a>
+                <a href="#" className="button button-5">
+                    <img className="labtests" src={labtests} alt="labtests"></img>
+                    <div>Lab Tests</div>
+                    <div>AT HOME</div>
+                </a>
+            </div>
+            <ImageCarousel />
+            <div className="title">
+                <h2>VITAMINS</h2>
+            </div>
+            <div className="product-container">
                 {/* Product Cards */}
                 <div className="product-card">
                     <img src={vitamin} alt="Vitamin b12" />
@@ -73,7 +75,7 @@ function mainpage(){
                 </div>
             </div>
         </div>
-        
+
     )
 }
 export default mainpage 
