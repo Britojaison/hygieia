3411e9b1f3ebcd89d2b6e9f4f8ced12bd33f8bac
Britojaison <britojaison123@gmail.com>
2024-08-24 09:03:33 +0530

    issue in pharm css title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Home from './pages/home';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Pharmacy from './pages/pharmacy'
-
+import Login from './pages/login'
 function App(){
   return (
     <>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pharm" element={<Pharmacy />} />
+        <Route path='/login' element={<Login />}/>
       </Routes>
       </BrowserRouter>
     </>
