faf8963afaeb7ebb62b3b00bd1167346f71db64d
NAYANA BENNY <ncmia2@gmail.com>
2024-09-01 15:24:08 +0530

    nothing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Home from './Pages/home'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Pharmacy from './Pages/pharmacy';
+import Login from './Pages/login';
 
-function App(){
+function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pharm" element={<Pharmacy />} />
+          <Route path='/login' element={<Login />} />
+
+        </Routes>
       </BrowserRouter>
     </>
   );
