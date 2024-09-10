1f4ba80f3ae3f74238383201f1441204e12385e4
Britojaison <britojaison123@gmail.com>
2024-09-10 12:31:19 +0530

    login with bugs

--- client/src/Pages/login.jsx
+++ client/src/Pages/login.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../assets/styles/login.css";
 import { FaUser,FaLock } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import axios from "axios"
+import { useNavigate,Link } from 'react-router-dom';
 
 function login(){
-    
+    const history= useNavigate()
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    async function handleSubmit (e) {
+        e.preventDefault();
+        // Implement login logic here
+        console.log(username)
+        await axios.post('http://localhost:5000/login',{username,password})
+        .then(res=>{
+            history("/")
+        })
+        .catch(res=>console.log(err));
+        
+    };
   return (
     <div className='body'>
     <div className='wrapper'>
-        <form action=''>
+        <form onSubmit={handleSubmit} action=''>
             <h1>Login</h1>
             <div className="input-box">
-                <input type='text' placeholder='Username' required />
+                <input 
+                type='text'
+                placeholder='Username'
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required />
                 <FaUser className='icon'/>
             </div>
             <div className="input-box">
-                <input type='Password' placeholder='Password' required />
+                <input 
+                type='Password' 
+                placeholder='Password' 
+                name='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required />
                 <FaLock className='icon' />
             </div>
 
