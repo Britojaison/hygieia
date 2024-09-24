db94d78f5b96b11708aef4b64875773c3bea118c
NAYANA BENNY <ncmia2@gmail.com>
2024-09-24 12:44:13 +0530

    signup and login page modified

--- client/src/Pages/login.jsx
+++ client/src/Pages/login.jsx
@@ -1,40 +1,43 @@
 import React from 'react'
 import "../assets/styles/login.css";
-import { FaUser,FaLock,FaArrowLeft } from "react-icons/fa";
+import { FaUser,FaLock,FaArrowLeft, FaPhone } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 function login(){
     
   return (
     <div className='body'>
-    <div className='wrapper'>
-    <Link to="/" className="back-arrow">
-        <FaArrowLeft /> 
-    </Link>
+      <div className='wrapper'>
+        <Link to="/" className="back-arrow">
+          <FaArrowLeft />
+        </Link>
         <form action=''>
-            <h1>Login</h1>
-            <div className="input-box">
-                <input type='text' placeholder='Username' required />
-                <FaUser className='icon'/>
-            </div>
-            <div className="input-box">
-                <input type='Password' placeholder='Password' required />
-                <FaLock className='icon' />
-            </div>
+          <h1>Login</h1>
 
-            <div className="remeber-forgot">
-                <label><input type='checkbox'/>Remember Me</label>
-                <a href='#'>Forgot Password?</a>
-            </div>
+          {/* Phone Number Field */}
+          <div className="input-box">
+            <input type='tel' placeholder='Phone Number' required />
+            <FaPhone className='icon'/>
+          </div>
 
-            <button type='submit'>Login</button>
+          {/* Password Field */}
+          <div className="input-box">
+            <input type='password' placeholder='Password' required />
+            <FaLock className='icon' />
+          </div>
 
-            <div className="register-link">
-                <p>Dont have an account?<Link className='signup' to="/signup">Register</Link></p>
-            </div>
+          <div className="remeber-forgot">
+            <label><input type='checkbox' />Remember Me</label>
+            <a href='#'>Forgot Password?</a>
+          </div>
+
+          <button type='submit'>Login</button>
+
+          <div className="register-link">
+            <p>Don't have an account? <Link className='signup' to="/signup">Register</Link></p>
+          </div>
         </form>
-
-    </div>
+      </div>
     </div>
   )
 }
--- client/src/Pages/signup.jsx
+++ client/src/Pages/signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "../assets/styles/signup.css";
-import { FaUser, FaLock, FaArrowLeft } from "react-icons/fa";
+import { FaUser, FaLock, FaArrowLeft, FaEnvelope, FaPhone, FaHome, FaCalendar, FaGraduationCap, FaBriefcase } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
@@ -13,40 +13,98 @@ const Signup = () => {
           <div>
             <form>
               <h1>Sign-Up as User</h1>
+
               <div className="input-box">
-                <input type='text' placeholder='Username' required />
-                <FaUser className='icon' />
+                <input type="text" placeholder="Full Name" required />
+                <FaUser className="icon" />
               </div>
+
               <div className="input-box">
-                <input type='password' placeholder='Password' required />
-                <FaLock className='icon' />
+                <input type="email" placeholder="Email" required />
+                <FaEnvelope className="icon" />
               </div>
-              <div className="remeber-forgot">
-                <label><input type='checkbox' />Remember Me</label>
-                <a href='#'>Forgot Password?</a>
+
+              <div className="input-box">
+                <input type="tel" placeholder="Phone Number" required />
+                <FaPhone className="icon" />
               </div>
-              <button type='submit'>Sign Up</button>
+
+              <div className="input-box">
+                <input type="text" placeholder="Address" required />
+                <FaHome className="icon" />
+              </div>
+
+              <div className="input-box">
+                <input type="date" placeholder="Date of Birth" required />
+                <FaCalendar className="icon" />
+              </div>
+
+              <div className="input-box">
+                <input type="password" placeholder="Password" required />
+                <FaLock className="icon" />
+              </div>
+
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" /> Remember Me
+                </label>
+                <a href="#">Forgot Password?</a>
+              </div>
+
+              <button type="submit">Sign Up</button>
             </form>
           </div>
+
         );
       case 'Doctor':
         return (
           <div>
             <form>
               <h1>Sign-Up as Doctor</h1>
+
               <div className="input-box">
-                <input type='text' placeholder='Doctor ID' required />
-                <FaUser className='icon' />
+                <input type="text" placeholder="Doctor Name" required />
+                <FaUser className="icon" />
               </div>
+
               <div className="input-box">
-                <input type='password' placeholder='Password' required />
-                <FaLock className='icon' />
+                <input type="email" placeholder="Email" required />
+                <FaEnvelope className="icon" />
               </div>
-              <div className="remeber-forgot">
-                <label><input type='checkbox' />Remember Me</label>
-                <a href='#'>Forgot Password?</a>
+
+              <div className="input-box">
+                <input type="tel" placeholder="Phone Number" required />
+                <FaPhone className="icon" />
               </div>
-              <button type='submit'>Sign Up</button>
+
+              <div className="input-box">
+                <input type="password" placeholder="Password" required />
+                <FaLock className="icon" />
+              </div>
+
+              <div className="input-box">
+                <input type="text" placeholder="Qualification" required />
+                <FaGraduationCap className="icon" />
+              </div>
+
+              <div className="input-box">
+                <input type="text" placeholder="Specialization" required />
+                <FaBriefcase className="icon" />
+              </div>
+
+              <div className="input-box">
+                <input type="text" placeholder="Address" required />
+                <FaHome className="icon" />
+              </div>
+
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" /> Remember Me
+                </label>
+                <a href="#">Forgot Password?</a>
+              </div>
+
+              <button type="submit">Sign Up</button>
             </form>
           </div>
         );
@@ -55,19 +113,35 @@ const Signup = () => {
           <div>
             <form>
               <h1>Sign-Up as Pharmacist</h1>
+
               <div className="input-box">
-                <input type='text' placeholder='Pharmacy ID' required />
-                <FaUser className='icon' />
+                <input type="text" placeholder="Pharmacist Name" required />
+                <FaUser className="icon" />
               </div>
+
               <div className="input-box">
-                <input type='password' placeholder='Password' required />
-                <FaLock className='icon' />
+                <input type="email" placeholder="Email" required />
+                <FaEnvelope className="icon" />
               </div>
-              <div className="remeber-forgot">
-                <label><input type='checkbox' />Remember Me</label>
-                <a href='#'>Forgot Password?</a>
+
+              <div className="input-box">
+                <input type="tel" placeholder="Phone Number" required />
+                <FaPhone className="icon" />
               </div>
-              <button type='submit'>Sign Up</button>
+
+              <div className="input-box">
+                <input type="password" placeholder="Password" required />
+                <FaLock className="icon" />
+              </div>
+
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" /> Remember Me
+                </label>
+                <a href="#">Forgot Password?</a>
+              </div>
+
+              <button type="submit">Sign Up</button>
             </form>
           </div>
         );
